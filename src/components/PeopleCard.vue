ebc9f65022282741145b3d3832084d5a65a61682
Hipy Cas <hipycas@gmail.com>
2023-09-07 20:38:49 +0200

    Clean vue conditional in template

--- src/components/PeopleCard.vue
+++ src/components/PeopleCard.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="bg-base-100 rounded-xl h-fit">
     <h2 class="text-center flex justify-center items-center font-bold text-xl m-6">Personas</h2>
-    <div v-if="usersStore.users.length > 0" class="form-control flex-row items-center">
-      <input
-        type="text"
-        v-model="searchTerm"
-        placeholder="Buscar"
-        class="input input-bordered flex-grow rounded-full w-auto m-4"
-      />
-      <button @click="() => (usersStore.users = [])" class="mr-5 btn btn-circle btn-ghost">
-        <IconX />
-      </button>
-    </div>
-    <div class="divider mx-auto w-11/12 m-2"></div>
-    <FileSelector v-if="usersStore.users.length === 0" />
-    <UsersList v-else :searchTerm="searchTerm" />
+    <template v-if="usersStore.users.length > 0">
+      <div class="form-control flex-row items-center">
+        <input
+          type="text"
+          v-model="searchTerm"
+          placeholder="Buscar"
+          class="input input-bordered flex-grow rounded-full w-auto m-4"
+        />
+        <button @click="niceFun" class="mr-5 btn btn-circle btn-ghost">
+          <IconX />
+        </button>
+      </div>
+      <UsersList :searchTerm="searchTerm" />
+      <div class="divider mx-auto w-11/12 m-2"></div>
+    </template>
+    <FileSelector v-else />
   </div>
 </template>
 
@@ -28,4 +30,10 @@ import { useUsersStore } from '@/stores/usersStore'
 const usersStore = useUsersStore()
 
 const searchTerm = ref('')
+
+const niceFun = () => {
+  console.info('nice')
+  usersStore.users = []
+  console.info('really nice')
+}
 </script>
